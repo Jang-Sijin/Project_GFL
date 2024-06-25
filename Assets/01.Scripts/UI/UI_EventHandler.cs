07c2a9aee090ac17ab928be7bbb2ce3ccf5795e8
J_SIJIN <76439399+Jang-Sijin@users.noreply.github.com>
2024-06-25 18:05:24 +0900

    240625_Update

--- Assets/01.Scripts/UI/UI_EventHandler.cs
+++ Assets/01.Scripts/UI/UI_EventHandler.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class UI_EventHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IDragHandler
 {
+    // [특징(캡슐화)]
+    // 이벤트 실행은 UI_EventHandler에서만 가능하다.
+    // 이벤트를 등록/해제는 외부에서 가능하다.
+
     public event Action OnClickHandler = null;
     public event Action OnPressHandler = null;
-    public event Action OnDragHandler = null;
+    public event Action OnDragHandler = null;   
 
     // 동일 오브젝트에서 포인터를 누르고 뗄 때 호출됩니다.
     public void OnPointerClick(PointerEventData eventData)
